4dcf1bb2be1c6b4b1edcdf9db6244a7fdf990dd1
Devin Devlin <devindevlin@Devins-MacBook-Pro.local>
2020-01-29 21:28:41 -0800

    first commit

---
+++ build/precache-manifest.d32da8af9b0521a94802dc7086ca9c20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ddd4e613e28f5c050e7",
+    "url": "/static/css/main.56b93223.chunk.css"
+  },
+  {
+    "revision": "9ddd4e613e28f5c050e7",
+    "url": "/static/js/main.9ddd4e61.chunk.js"
+  },
+  {
+    "revision": "ee927a90d76d6ce20845",
+    "url": "/static/js/1.ee927a90.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7940e6ce3d1c814582d82554510dff6b",
+    "url": "/static/media/logo.7940e6ce.png"
+  },
+  {
+    "revision": "efd54d55d82366876c8b1801ef6dfa33",
+    "url": "/index.html"
+  }
+];
